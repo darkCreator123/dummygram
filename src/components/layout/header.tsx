7c410b20b84c711d33560548d5526f5f827e5e58
darkCreator123 <147541344+darkCreator123@users.noreply.github.com>
2023-10-11 12:20:55 +0530

    Clone of instagram

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../navbar'
+
+const Header = () => {
+  return (
+    <header className="bg-white border-[1px] border-gray-300 sticky top-0 z-20">
+      <div className="max-w-5x2 mx-auto">
+        <Navbar />
+      </div>
+    </header>
+  )
+}
+
+export default Header
